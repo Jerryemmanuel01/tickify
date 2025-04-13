fadb7fb9c80e6e34229474589807d20da55c80f5
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-04-13 02:22:11 +0100

    Fix: all commented out

--- server/api/index.js
+++ server/api/index.js
@@ -1,31 +1,32 @@
 import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import mongoose from "mongoose";
-import authRouter from "../routes/authRoutes.js";
-import taskRouter from "../routes/taskRoutes.js";
+// import cors from "cors";
+// import helmet from "helmet";
+// import mongoose from "mongoose";
+// import authRouter from "../routes/authRoutes.js";
+// import taskRouter from "../routes/taskRoutes.js";
 import serverless from 'serverless-http';
 
 const app = express();
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(helmet());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(process.env.DATABASE_URI)
-  .then(() => {
-    console.log("Database Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.DATABASE_URI)
+//   .then(() => {
+//     console.log("Database Connected");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-app.get("/api/hello", (req, res) => {
-  res.send("getting the server okay");
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "If you see this, it's working!" });
 });
-app.use("/api/auth", authRouter)
-app.use("/api/task", taskRouter)
+
+// app.use("/api/auth", authRouter)
+// app.use("/api/task", taskRouter)
 
 
 export default serverless(app);
