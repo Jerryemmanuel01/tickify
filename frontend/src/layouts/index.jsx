daabba3ae93b0fd411d29602c8d57f7786cb7eea
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-04-15 00:42:39 +0100

    fix: checking errors

--- frontend/src/layouts/index.jsx
+++ frontend/src/layouts/index.jsx
@@ -7,7 +7,7 @@ const index = () => {
       style={{
         paddingBottom: "env(safe-area-inset-bottom)",
       }}
-      className="w-full overflow-hidden min-h-[100vh] bg-gradient-to-br from-primary to-black pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)] "
+      className="w-full min-h-[100vh] bg-gradient-to-br from-primary to-black pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)] "
     >
       <ScrollRestoration />
       <Navbar />
