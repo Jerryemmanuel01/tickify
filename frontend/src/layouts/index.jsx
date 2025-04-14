a930985e46f0404b15bfee2b54d1cc0f272bff36
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-04-15 00:39:47 +0100

    fix: css adjusted

--- frontend/src/layouts/index.jsx
+++ frontend/src/layouts/index.jsx
@@ -3,7 +3,12 @@ import Navbar from "../components/Navbar"
 
 const index = () => {
   return (
-    <section className="w-full min-h-[105vh] bg-gradient-to-br from-primary to-black pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)] ">
+    <section
+      style={{
+        paddingBottom: "env(safe-area-inset-bottom)",
+      }}
+      className="w-full overflow-hidden min-h-[100vh] bg-gradient-to-br from-primary to-black pb-[env(safe-area-inset-bottom)] pt-[env(safe-area-inset-top)] "
+    >
       <ScrollRestoration />
       <Navbar />
       <Outlet />
